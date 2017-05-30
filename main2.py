eb6b037df6329d9ded733555761da97776a36b21
esteban <marcoestebansilva@gmail.com>
2017-05-30 15:28:33 +0200

    first

--- main2.py
+++ main2.py
@@ -3,6 +3,7 @@
 import pyaudio
 import wave
 import sys
+import numpy as np
 
 CHUNK = 1024
 
@@ -15,11 +16,23 @@ stream = p.open(format=p.get_format_from_width(wf.getsampwidth()),
                 rate=wf.getframerate(),
                 output=True)
 
-data = wf.readframes(CHUNK)
+bpm = 120
+beat = 60./bpm  # seconds
 
+fs = wf.getframerate()
+s_son = wf.getnframes()
+s_beat = int(fs*beat)
+s_sil = s_beat-s_son
+
+s = np.zeros(s_sil, dtype=np.int16).tobytes()
+
+data = wf.readframes(CHUNK)
 while data != '':
     stream.write(data)
     data = wf.readframes(CHUNK)
+    if wf.tell() == wf.getnframes():
+        wf.rewind()
+        stream.write(s)
 
 stream.stop_stream()
 stream.close()
