e33112b3a0ef224bc03b516af90bff410a48121d
esteban <marcoestebansilva@gmail.com>
2017-05-30 16:53:14 +0200

    working

--- main2.py
+++ main2.py
@@ -1,14 +1,19 @@
-"""PyAudio Example: Play a WAVE file."""
-
 import pyaudio
 import wave
 import sys
 import numpy as np
+from scipy.io import wavfile
+import os
 
 CHUNK = 1024
 
-wf = wave.open('sounds/4d.wav', 'rb')
+fs, d = wavfile.read('sounds/4d.wav')
+# first beat
+# wavfile.write('sounds/4dH.wav', fs, 4*d)
+# derivate beat
+# wavfile.write('sounds/4dl.wav', fs, d/2)
 
+wf = wave.open('sounds/4d.wav', 'rb')
 p = pyaudio.PyAudio()
 
 stream = p.open(format=p.get_format_from_width(wf.getsampwidth()),
@@ -27,14 +32,18 @@ s_sil = s_beat-s_son
 s = np.zeros(s_sil, dtype=np.int16).tobytes()
 
 data = wf.readframes(CHUNK)
-while data != '':
-    stream.write(data)
-    data = wf.readframes(CHUNK)
-    if wf.tell() == wf.getnframes():
-        wf.rewind()
-        stream.write(s)
+try:
+    while data != '':
+        stream.write(data)
+        data = wf.readframes(CHUNK)
+        if wf.tell() == wf.getnframes():
+            wf.rewind()
+            stream.write(s)
 
-stream.stop_stream()
-stream.close()
-
-p.terminate()
+except KeyboardInterrupt:
+    stream.stop_stream()
+    stream.close()
+    p.terminate()
+    # os.remove('sounds/4dH.wav')
+    # os.remove('sounds/4dl.wav')
+    print("end")
