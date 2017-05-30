247be51b3170793e74604ec850f38b464e53ae95
esteban <marcoestebansilva@gmail.com>
2017-05-30 14:10:10 +0200

    first

--- main.py
+++ main.py
@@ -4,15 +4,15 @@ import sounddevice as sd
 
 bpm = 120
 beat = 60./bpm  # seconds
-fs, data = wavfile.read('sounds/4d.wav')
+fs, d = wavfile.read('sounds/4d.wav')
 
-s_son = len(data)
+s_son = len(d)
 s_beat = int(fs*beat)
 s_sil = s_beat-s_son
 
-print(data)
-silence = np.random.uniform(-1, 1, s_sil).astype(np.int16)
-v = np.concatenate((2*data, silence, data, silence, data, silence, data, silence), axis=0)
+print(d)
+s = np.random.uniform(-1, 1, s_sil).astype(np.int16)
+v = np.concatenate((2*d, s, d, s, d, s, d, s), axis=0)
 print(v)
 while True:
     sd.play(v, fs, blocking=True)
